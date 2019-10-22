ce66fb27c440040aeb8412c52db7d4c24eaaa859
ashdunn <31973458+ashdunn@users.noreply.github.com>
2019-10-21 23:11:16 -0400

    clickable

---
+++ Assets/DirectorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectorScript : MonoBehaviour
+{
+
+    private GameObject selected;
+    public Material human;
+    public Material agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit/*, 100*/))
+            {
+
+                if (hit.transform.tag == "Player")
+                {
+                    if (selected != null)
+                        selected.GetComponent<Renderer>().material = agent;
+                    selected = hit.transform.gameObject;
+                    selected.GetComponent<Renderer>().material = human;
+                }
+
+
+            }
+
+
+        }
+    }
+}
